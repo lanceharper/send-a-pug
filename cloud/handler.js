f301c99d496cf649ebdd5e0028620249727eda8e
Lance Harper <lance.harper@gmail.com>
2019-02-23 16:08:31 -0800

    generate token for new/existing user from user pool using twitch token

--- cloud/handler.js
+++ cloud/handler.js
@@ -1,8 +1,10 @@
 const AWS = require("aws-sdk");
 const jwt = require("jsonwebtoken");
 const fetch = require("node-fetch");
-const uuidv1 = require("uuid/v1");
 
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+
+const newUserPassword = "f00BarBaz";
 const verifyAndDecode = twitchToken => {
   try {
     const secret = process.env.JERK_TWITCH_SECRET;
@@ -15,16 +17,65 @@ const verifyAndDecode = twitchToken => {
   }
 };
 
-const createCognitoUserFromTwitchUser = async twitchUser => {
-  let cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
-    apiVersion: "2016-04-18"
-  });
-
+const getCognitoUserFromTwitchUser = async twitchUser => {
   let params = {
     UserPoolId: process.env.USER_POOL_ID,
-    Username: uuidv1(),
+    Username: twitchUser["_id"]
+  };
+
+  const createdCognitoUser = await cognitoIdentityServiceProvider
+    .adminGetUser(params)
+    .promise();
+  return createdCognitoUser;
+};
+
+const initiateCognitoAuth = async cognitoUser => {
+  let params = {
+    AuthFlow: "ADMIN_NO_SRP_AUTH",
+    UserPoolId: process.env.USER_POOL_ID,
+    ClientId: process.env.USER_POOL_CLIENT_ID,
+    AuthParameters: {
+      USERNAME: cognitoUser.Username,
+      PASSWORD: newUserPassword
+    }
+  };
+
+  try {
+    const createdInitiatedAuthUserResponse = await cognitoIdentityServiceProvider
+      .adminInitiateAuth(params)
+      .promise();
+    return createdInitiatedAuthUserResponse;
+  } catch (createdInitiatedAuthError) {
+    console.error("createdInitiatedAuthError", createdInitiatedAuthError);
+    return cognitoUser;
+  }
+};
+
+const respondToAuthChallenge = async (cognitoUser, initiatedAuthResponse) => {
+  let params = {
+    ClientId: process.env.USER_POOL_CLIENT_ID,
+    ChallengeName: "NEW_PASSWORD_REQUIRED",
+    ChallengeResponses: {
+      USERNAME: cognitoUser.Username,
+      NEW_PASSWORD: newUserPassword
+    },
+    Session: initiatedAuthResponse.Session,
+    UserPoolId: process.env.USER_POOL_ID
+  };
+
+  const adminRespondToAuthChallenge = await cognitoIdentityServiceProvider
+    .adminRespondToAuthChallenge(params)
+    .promise();
+
+  return adminRespondToAuthChallenge;
+};
+
+const createCognitoUserFromTwitchUser = async twitchUser => {
+  let params = {
+    UserPoolId: process.env.USER_POOL_ID,
+    Username: twitchUser["_id"],
     MessageAction: "SUPPRESS", // Do not send welcome email
-    TemporaryPassword: "f00bar",
+    TemporaryPassword: newUserPassword,
     UserAttributes: [
       { Name: "preferred_username", Value: twitchUser.display_name }
     ]
@@ -33,7 +84,6 @@ const createCognitoUserFromTwitchUser = async twitchUser => {
   const createdCognitoUser = await cognitoIdentityServiceProvider
     .adminCreateUser(params)
     .promise();
-  console.log("createdCognitoUser", createdCognitoUser);
   return createdCognitoUser;
 };
 
@@ -65,15 +115,41 @@ module.exports.hello = async (event, context) => {
     };
   };
 
-  const payload = verifyAndDecode(JSON.parse(event.body).twitchToken);
+  try {
+    const payload = verifyAndDecode(JSON.parse(event.body).twitchToken);
 
-  const twitchUser = await getTwitchUser(payload.user_id);
+    const twitchUser = await getTwitchUser(payload.user_id);
 
-  const cognitoUser = await createCognitoUserFromTwitchUser(twitchUser);
+    let cognitoUser = null;
 
-  console.log("twitchUser", twitchUser);
+    try {
+      cognitoUser = await getCognitoUserFromTwitchUser(twitchUser);
+    } catch ({ code }) {
+      if (code === "UserNotFoundException") {
+        cognitoUser = await createCognitoUserFromTwitchUser(twitchUser);
+      } else {
+        return response(500, JSON.stringify(error));
+      }
+    }
 
-  if (payload.err) return response(401, JSON.stringify(payload));
+    if (cognitoUser.User) {
+      cognitoUser = cognitoUser.User;
+    }
 
-  return response(200, cognitoUser);
+    if (cognitoUser.UserStatus === "FORCE_CHANGE_PASSWORD") {
+      const cognitoAuthResponse = await initiateCognitoAuth(cognitoUser);
+      const challengeResponse = await respondToAuthChallenge(
+        cognitoUser,
+        cognitoAuthResponse
+      );
+      return response(200, challengeResponse.AuthenticationResult);
+    } else if (cognitoUser.UserStatus === "CONFIRMED") {
+      const cognitoAuthResponse = await initiateCognitoAuth(cognitoUser);
+      return response(200, cognitoAuthResponse.AuthenticationResult);
+    }
+
+    return response(500, JSON.stringify(payload));
+  } catch (ohNoError) {
+    return response(500, JSON.stringify(ohNoError));
+  }
 };
