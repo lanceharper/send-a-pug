22fa50e4d13f833ade52208deaf144df43ece622
Lance Harper <lance.harper@gmail.com>
2019-03-01 17:16:11 -0800

    process records

--- cloud/aggregateJerk.js
+++ cloud/aggregateJerk.js
@@ -12,70 +12,124 @@ exports.handler = async event => {
 
   const streamerToLeaderboardEntryMap = new Map();
 
-  const output = event.records.forEach(record => {
+  let recordOutput = [];
+  event.records.forEach(record => {
     try {
       const recordData = Buffer.from(record.data, "base64");
 
-      const { streamer_id, user_id, PUG_COUNT } = recordData.toString("ascii");
+      let { streamer_id, user_id, PUG_COUNT, ...restRecord } = JSON.parse(
+        recordData.toString("ascii")
+      );
+
+      console.log("new record", {
+        streamer_id,
+        user_id,
+        PUG_COUNT,
+        restRecord
+      });
 
       if (!streamerToLeaderboardEntryMap.has(streamer_id)) {
         streamerToLeaderboardEntryMap.set(
           streamer_id,
-          new Map([user_id, PUG_COUNT])
+          new Map([[user_id, PUG_COUNT]])
         );
       } else {
         streamerToLeaderboardEntryMap.get(streamer_id).set(user_id, PUG_COUNT);
       }
+
       success++;
-      return {
+      recordOutput.push({
         recordId: record.recordId,
         result: "Ok"
-      };
+      });
     } catch (err) {
       failure++;
       console.error(err);
-      return {
+      recordOutput.push({
         recordId: record.recordId,
         result: "DeliveryFailed"
-      };
+      });
     }
   });
 
+  console.log("streamerToLeaderboardEntryMap", streamerToLeaderboardEntryMap);
+
   await streamerToLeaderboardEntryMap.forEach(
     async (userToScoreMap, streamer_id) => {
-      const previousLeaderboardForSteamerDDBParams = {
-        TableName: process.env.LEADERS_TABLE,
-        Key: {
-          streamer_id: { S: streamer_id }
-        },
-        ProjectionExpression: "streamer_id, leaderboard"
-      };
+      try {
+        const previousLeaderboardForSteamerDDBParams = {
+          TableName: process.env.LEADERS_TABLE,
+          Key: {
+            streamer_id: { S: streamer_id }
+          },
+          ProjectionExpression: "streamer_id, leaderboard"
+        };
 
-      const { Item: previousLeaderboardDDBItem } = await ddb
-        .getItem(previousLeaderboardForSteamerDDBParams)
-        .promise();
+        const { Item: previousLeaderboardDDBItem } = await ddb
+          .getItem(previousLeaderboardForSteamerDDBParams)
+          .promise();
 
-      const {
-        player,
-        score: previousScore
-      } = AWS.DynamoDB.Converter.unmarshall(previousLeaderboardDDBItem);
+        const previousLeaderboardForStreamer = previousLeaderboardDDBItem
+          ? AWS.DynamoDB.Converter.unmarshall(previousLeaderboardDDBItem)
+          : {
+              streamer_id,
+              leaderboard: []
+            };
 
-      // If there are previous scores for a streamer's user,
-      // increase their total score by the amount returned over the last time window
-      const updatedScoresForStreamer = {
-        streamer_id,
-        leaderboard: userToScoreMap.forEach((PUG_COUNT, user_id) => ({
-          player: user_id,
-          score: player ? previousScore + PUG_COUNT : PUG_COUNT
-        }))
-      };
+        console.log(
+          "previousLeaderboardForStreamer",
+          previousLeaderboardForStreamer
+        );
 
-      updatedScoresForStreamerPutParams = {
-        TableName: process.env.LEADERS_TABLE,
-        Item: AWS.DynamoDB.Converter.marshall(updatedScoresForStreamer)
-      };
+        let leaderboard = [];
 
-      await ddb.putItem(updatedScoresForStreamerPutParams).promise();
+        userToScoreMap.forEach((PUG_COUNT, user_id) => {
+          console.log("PUG_COUNT, user_id", { PUG_COUNT, user_id });
+
+          console.log(
+            "previousLeaderboardForStreamer.leaderboard",
+            previousLeaderboardForStreamer.leaderboard
+          );
+
+          const previousLeaderboardPlayerEntry = previousLeaderboardForStreamer.leaderboard.find(
+            ({ player }) => user_id === player
+          );
+
+          console.log(
+            "previousLeaderboardPlayerEntry",
+            previousLeaderboardPlayerEntry
+          );
+
+          leaderboard.push({
+            player: user_id,
+            score: previousLeaderboardPlayerEntry
+              ? previousLeaderboardPlayerEntry.score + PUG_COUNT
+              : PUG_COUNT
+          });
+        });
+
+        // If there are previous scores for a streamer's user,
+        // increase their total score by the amount returned over the last time window
+        const updatedScoresForStreamer = {
+          streamer_id,
+          leaderboard
+        };
+
+        console.log("updatedScoresForStreamer", updatedScoresForStreamer);
+
+        updatedScoresForStreamerPutParams = {
+          TableName: process.env.LEADERS_TABLE,
+          Item: AWS.DynamoDB.Converter.marshall(updatedScoresForStreamer)
+        };
+
+        console.log(
+          "updatedScoresForStreamerPutParams",
+          updatedScoresForStreamerPutParams
+        );
+        await ddb.putItem(updatedScoresForStreamerPutParams).promise();
+      } catch (error) {
+        console.error(`Failed to update table.`, error);
+      }
     }
   );
 
@@ -83,7 +137,9 @@ exports.handler = async event => {
     `Successfully delivered records ${success}, Failed delivered records ${failure}.`
   );
 
+  console.log("output", recordOutput);
+
   return {
-    records: output
+    records: recordOutput
   };
 };
